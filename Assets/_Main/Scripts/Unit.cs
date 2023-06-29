80fd8ddebf7667750c61e3c72dcec6ff034dd3ad
MaxScarp <massy1997.lavoro@gmail.com>
2023-06-29 18:48:26 +0200

    Development
    Mouse Raycast

--- Assets/_Main/Scripts/Unit.cs
+++ Assets/_Main/Scripts/Unit.cs
@@ -7,14 +7,14 @@ public class Unit : MonoBehaviour
     private void Update()
     {
         float stoppingDistance = 0.1f;
-        if(Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
         }
 
-        if(Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.T))
         {
             Move(new Vector3(4, 0, 4));
         }
