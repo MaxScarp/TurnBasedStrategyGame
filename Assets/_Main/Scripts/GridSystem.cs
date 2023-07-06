45460a474cb71d1ebcd44b909aa397b3f3f500fb
MaxScarp <massy1997.lavoro@gmail.com>
2023-07-06 18:47:48 +0200

    Development
    Grid Object

--- Assets/_Main/Scripts/GridSystem.cs
+++ Assets/_Main/Scripts/GridSystem.cs
@@ -5,6 +5,7 @@ public class GridSystem
     private int width;
     private int height;
     private float cellSize;
+    private GridObject[,] gridObjectArray;
 
     public GridSystem(int width, int height, float cellSize)
     {
@@ -12,11 +13,14 @@ public class GridSystem
         this.height = height;
         this.cellSize = cellSize;
 
+        gridObjectArray = new GridObject[width, height];
+
         for (int x = 0; x < this.width; x++)
         {
             for (int z = 0; z < this.height; z++)
             {
-                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z) + Vector3.right * 0.2f, Color.green, 1000f);
+                GridPosition gridPosition = new GridPosition(x, z);
+                gridObjectArray[x, z] = new GridObject(this, gridPosition);
             }
         }
     }
@@ -29,5 +33,16 @@ public class GridSystem
     public GridPosition GetGridPosition(Vector3 worldPosition)
     {
         return new GridPosition(Mathf.RoundToInt(worldPosition.x / cellSize), Mathf.RoundToInt(worldPosition.z / cellSize));
+    }
+
+    public void CreateDebugObjects(Transform debugPrefab)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                GameObject.Instantiate(debugPrefab, GetWorldPosition(x, z), Quaternion.identity);
+            }
+        }
     }
 }
