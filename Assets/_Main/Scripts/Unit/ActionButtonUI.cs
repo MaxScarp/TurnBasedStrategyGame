53df9a4bbc9d73db544e056c8e351580a8a7e531
MaxScarp <massy1997.lavoro@gmail.com>
2023-07-22 22:06:42 +0200

    Development
    Selected Action UI Visual

--- Assets/_Main/Scripts/Unit/ActionButtonUI.cs
+++ Assets/_Main/Scripts/Unit/ActionButtonUI.cs
@@ -6,14 +6,25 @@ public class ActionButtonUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textMeshProUGUI;
     [SerializeField] private Button button;
+    [SerializeField] private GameObject selectedGameObject;
+
+    private BaseAction baseAction;
 
     public void SetBaseAction(BaseAction baseAction)
     {
+        this.baseAction = baseAction;
+
         textMeshProUGUI.text = baseAction.GetActionName().ToUpper();
 
         button.onClick.AddListener(() =>
         {
             UnitActionSystem.Instance.SetSelectedAction(baseAction);
         });
+    }
+
+    public void UpdateSelectedVisual()
+    {
+        BaseAction selectedBaseAction = UnitActionSystem.Instance.GetSelectedAction();
+        selectedGameObject.SetActive(selectedBaseAction == baseAction);
     }
 }
